208fc45286a90424437c9eb3896b8284d7c6a8d5
kss418 <jis1595@naver.com>
2026-03-07 18:54:03 +0900

    make_output_path(), make_input_path() 추가

--- include/judge_server/testcase_downloader.hpp
+++ include/judge_server/testcase_downloader.hpp
@@ -3,18 +3,31 @@
 #include "common/error_code.hpp"
 #include "db/db_connection.hpp"
 
+#include <cstdint>
 #include <expected>
 #include <filesystem>
 
 class testcase_downloader{
 public:
     static std::expected<testcase_downloader, error_code> create(db_connection connection);
+    std::expected<void, error_code> download_one(
+        std::int64_t problem_id,
+        std::int32_t order
+    );
 
 private:
     explicit testcase_downloader(
         db_connection connection,
         std::filesystem::path root_path
     );
+    std::filesystem::path make_input_path(
+        std::int64_t problem_id,
+        std::int32_t order
+    ) const;
+    std::filesystem::path make_output_path(
+        std::int64_t problem_id,
+        std::int32_t order
+    ) const;
 
     db_connection connection_;
     std::filesystem::path root_path_;
--- src/judge_server/testcase_downloader.cpp
+++ src/judge_server/testcase_downloader.cpp
@@ -1,8 +1,12 @@
 #include "judge_server/testcase_downloader.hpp"
 
 #include "common/env_utility.hpp"
+#include "common/file_utility.hpp"
+#include "db/testcase_service.hpp"
 
 #include <filesystem>
+#include <string>
+#include <system_error>
 #include <utility>
 
 testcase_downloader::testcase_downloader(
@@ -29,3 +33,51 @@ std::expected<testcase_downloader, error_code> testcase_downloader::create(db_co
     return testcase_downloader(std::move(connection), std::move(root_path));
 }
 
+std::filesystem::path testcase_downloader::make_input_path(
+    std::int64_t problem_id, std::int32_t order
+) const{
+    return root_path_ / std::to_string(problem_id) / (std::to_string(order) + ".in");
+}
+
+std::filesystem::path testcase_downloader::make_output_path(
+    std::int64_t problem_id, std::int32_t order
+) const{
+    return root_path_ / std::to_string(problem_id) / (std::to_string(order) + ".out");
+}
+
+std::expected<void, error_code> testcase_downloader::download_one(
+    std::int64_t problem_id, std::int32_t order
+){
+    const auto testcase_exp = testcase_service::get_testcase(connection_, problem_id, order);
+    if(!testcase_exp){
+        return std::unexpected(testcase_exp.error());
+    }
+
+    const std::filesystem::path input_path = make_input_path(problem_id, order);
+    const std::filesystem::path output_path = make_output_path(problem_id, order);
+    std::error_code create_directories_ec;
+    std::filesystem::create_directories(input_path.parent_path(), create_directories_ec);
+    if(create_directories_ec){
+        return std::unexpected(
+            error_code::create(error_code::map_errno(create_directories_ec.value()))
+        );
+    }
+
+    const auto create_input_exp = file_utility::create_file(
+        input_path,
+        testcase_exp->testcase_input
+    );
+    if(!create_input_exp){
+        return std::unexpected(create_input_exp.error());
+    }
+
+    const auto create_output_exp = file_utility::create_file(
+        output_path,
+        testcase_exp->testcase_output
+    );
+    if(!create_output_exp){
+        return std::unexpected(create_output_exp.error());
+    }
+
+    return {};
+}
